b6c6d1003674ace161768972320af125fa4aeb70
heeraknpsingh <heerasingh.mca07@gmail.com>
2024-06-17 09:16:15 +0400

    Adding postgres

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/ToDoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/ToDoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_ex1XuACkKBBu4DlAAD-@deploy-database-heerasingh-e6bb.j.aivencloud.com:28242/defaultdb?sslmode=require"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
